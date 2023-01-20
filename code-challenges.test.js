4388563acaadb684639b786a638142ea45153f64
priceymineral <tudaeva@gmail.com>
2023-01-20 15:36:26 -0800

    completed code-challenges.test.js

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -67,6 +67,18 @@ const peopleInfo = (peopleArr) => {
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
+// TEST PSEUDO
+// describe - come up with a fxn name that performs the following 
+// it - returns an array of remainders of only the numbers divided by 3
+// expect - use the the constants provided to test the expected result against the actual result of your fxn
+// error - check for the "good" error: "<fxn name> is not defined"
+
+describe("onlyRemainders", () => {
+  it("returns an array with the remainders of the numbers in the input array divided by 3", () => {
+    expect(onlyRemainders(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(onlyRemainders(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
 
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
@@ -74,14 +86,61 @@ const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
 // b) Create the function that makes the test pass.
+// FXN PSEUDO
+// create a fxn that takes an array as an argument
+  // initialize an empty result array
+  // iterate over the input array
+    // if the current element is a number
+      // push the remainder of the current number modulo'd by 3 to the result array
+  // return the result array
+
+const onlyRemainders = (arr) => {
+  let remainders = []
+  for (const element of arr) {
+    // console.log(typeof element)
+    if (typeof element === "number") {
+      // console.log('in if')
+      // let remainder = element % 3
+      remainders.push(element % 3)
+    }
+  }
+  return remainders
+}
+
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
+// TEST PSEUDO
+// describe - name a fxn that 
+// it - returns the sum of all input numbers cubed
+// expect - use the constants provided to test the actual fxn output against the expected result
+// // error - check for the "good" error: "<fxn name> is not defined"
 
+describe("sumOfCubes", () => {
+  it("returns the sum of all the input numbers cubed", () => {
+    expect(sumOfCubes(cubeAndSum1)).toEqual(99)
+    expect(sumOfCubes(cubeAndSum2)).toEqual(1125)
+  })
+})
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
 // b) Create the function that makes the test pass.
+// FXN PSEUDO
+// sumOfCubes takes one array argument
+  // initialize a result varible
+  // iterate over input array
+    // add the result of the current number cubed to the result
+
+  // return the result
+
+const sumOfCubes = (arr) => {
+  let total = 0
+  for (const num of arr) {
+    total+=(num**3)
+  }
+  return total
+}
